e68ed576d23305ef2e17386e2f742f43209951a1
shoaib patel <100205704+pshoaib@users.noreply.github.com>
2022-06-29 20:28:03 +0530

    Add files via upload

---
+++ Hotstar_II/script.js/index.js
@@ -0,0 +1,68 @@
+let arr2=[
+  
+    {Title: 'The Chronicles of Narnia: The Lion, the Witch and the Wardrobe', Year: '2005', imdbID: 'tt0363771', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMTc0NTUwMTU5OV5BMl5BanBnXkFtZTcwNjAwNzQzMw@@._V1_SX300.jpg'}
+   , 
+   {Title: 'The Lion King', Year: '2019', imdbID: 'tt6105098', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjM3ODY3Njc5Ml5BMl5BanBnXkFtZTgwMjQ5NjM5MTI@._V1_SX300.jpg'}
+   ,
+    {Title: 'Lion', Year: '2016', imdbID: 'tt3741834', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjA3NjkzNjg2MF5BMl5BanBnXkFtZTgwMDkyMzgzMDI@._V1_SX300.jpg'}
+   , 
+   {Title: "The Lion King 2: Simba's Pride", Year: '1998', imdbID: 'tt0120131', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg'}
+   
+   ,
+    {Title: 'The Lion in Winter', Year: '1968', imdbID: 'tt0063227', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMTkzNzYyMzA5N15BMl5BanBnXkFtZTgwODcwODQ3MDI@._V1_SX300.jpg'}
+   
+   ,
+    {Title: 'The Lion King', Year: '2019', imdbID: 'tt6105098', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg'} 
+   , 
+   {Title: 'King Arthur: Legend of the Sword', Year: '2017', imdbID: 'tt1972591', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjM3ODY3Njc5Ml5BMl5BanBnXkFtZTgwMjQ5NjM5MTI@._V1_SX300.jpg'}
+   ,
+    {Title: 'King Arthur: Legend of the Sword', Year: '2017', imdbID: 'tt1972591', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjM3ODY3Njc5Ml5BMl5BanBnXkFtZTgwMjQ5NjM5MTI@._V1_SX300.jpg'}
+   ,
+    {Title: 'The Last King of Scotland', Year: '2006', imdbID: 'tt0455590', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjM1NTkxNjkzMl5BMl5BanBnXkFtZTgwNDgwMDAxMzE@._V1_SX300.jpg'}
+   , 
+   {Title: 'Godzilla: King of the Monsters', Year: '2019', imdbID: 'tt3741700', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjA3NjkzNjg2MF5BMl5BanBnXkFtZTgwMDkyMzgzMDI@._V1_SX300.jpg'}
+   , 
+   {Title: 'King Arthur', Year: '2004', imdbID: 'tt0349683', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMTk4MTk1NjI0OV5BMl5BanBnXkFtZTcwNDQxOTUyMQ@@._V1_SX300.jpg'}
+   ,
+    {Title: 'The Scorpion King', Year: '2002', imdbID: 'tt0277296', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjkxNTAwNTQ0M15BMl5BanBnXkFtZTYwMTQwMjM3._V1_SX300.jpg'}
+   ,
+    {Title: 'The Lion King', Year: '2019', imdbID: 'tt6105098', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg'} 
+   , 
+   {Title: 'King Arthur: Legend of the Sword', Year: '2017', imdbID: 'tt1972591', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjM3ODY3Njc5Ml5BMl5BanBnXkFtZTgwMjQ5NjM5MTI@._V1_SX300.jpg'}
+   ,
+    {Title: 'King Arthur: Legend of the Sword', Year: '2017', imdbID: 'tt1972591', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjM3ODY3Njc5Ml5BMl5BanBnXkFtZTgwMjQ5NjM5MTI@._V1_SX300.jpg'}
+   ,
+    {Title: 'The Last King of Scotland', Year: '2006', imdbID: 'tt0455590', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjM1NTkxNjkzMl5BMl5BanBnXkFtZTgwNDgwMDAxMzE@._V1_SX300.jpg'}
+   , 
+   {Title: 'Godzilla: King of the Monsters', Year: '2019', imdbID: 'tt3741700', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjA3NjkzNjg2MF5BMl5BanBnXkFtZTgwMDkyMzgzMDI@._V1_SX300.jpg'}
+   , 
+   {Title: 'King Arthur', Year: '2004', imdbID: 'tt0349683', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMTk4MTk1NjI0OV5BMl5BanBnXkFtZTcwNDQxOTUyMQ@@._V1_SX300.jpg'}
+    ,
+   {Title: 'Godzilla: King of the Monsters', Year: '2019', imdbID: 'tt3741700', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMjA3NjkzNjg2MF5BMl5BanBnXkFtZTgwMDkyMzgzMDI@._V1_SX300.jpg'}
+   , 
+   {Title: 'King Arthur', Year: '2004', imdbID: 'tt0349683', Type: 'movie', Poster: 'https://m.media-amazon.com/images/M/MV5BMTk4MTk1NjI0OV5BMl5BanBnXkFtZTcwNDQxOTUyMQ@@._V1_SX300.jpg'}
+       ]
+   
+       arr2.map(function(elem,index,aray){
+       
+     let low = document.getElementById("lowest");
+      
+        
+        let div = document.createElement("div")
+        
+        let title = document.createElement("h3")
+           title.innerText=`Title : ${elem.Title}`
+           
+        let tit = document.createElement("h3")
+           tit.innerText=` Release Year :${elem.Year}`
+   
+           let poster = document.createElement("img")
+           poster.setAttribute("src",elem.Poster)
+        
+   
+           div.append(title,tit,poster)
+   
+         lowest.append(div)
+       })
+
+   
